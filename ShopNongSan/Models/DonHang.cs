1daf955a2e77668311b5cc1d65ae93a55401e63a
TinhLam <1150080038@sv.hcmunre.edu.vn>
2025-09-06 23:59:49 +0700

    chinh donhang.cs

--- ShopNongSan/Models/DonHang.cs
+++ ShopNongSan/Models/DonHang.cs
@@ -1,23 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace ShopNongSan.Models;
+﻿using ShopNongSan.Models;
 
 public partial class DonHang
 {
     public long Id { get; set; }
-
     public string MaDonHang { get; set; } = null!;
-
     public Guid TaiKhoanId { get; set; }
-
     public decimal TongTien { get; set; }
-
     public string TrangThai { get; set; } = null!;
 
+    // >>> Thêm các trường giao hàng/thanh toán
+    public string? HoTen { get; set; }
+    public string? SoDienThoai { get; set; }
+    public string? DiaChi { get; set; }
+    public string? GhiChu { get; set; }
+    public string? PhuongThucThanhToan { get; set; }
+
     public virtual ICollection<DoiTra> DoiTras { get; set; } = new List<DoiTra>();
-
     public virtual ICollection<DonHangChiTiet> DonHangChiTiets { get; set; } = new List<DonHangChiTiet>();
-
     public virtual TaiKhoan TaiKhoan { get; set; } = null!;
 }
