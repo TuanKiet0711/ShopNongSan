f9b9528161426de5c14bd0a7a57bc2bb050d4dbc
TinhLam <1150080038@sv.hcmunre.edu.vn>
2025-09-16 18:07:10 +0700

    sua trang admin thanh tong quan co bao cao doanh thu

--- ShopNongSan/Areas/Admin/Controllers/HomeController.cs
+++ ShopNongSan/Areas/Admin/Controllers/HomeController.cs
@@ -1,15 +1,33 @@
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using ShopNongSan.Models;
-using System.Diagnostics;
+using System.Linq;
 
 namespace ShopNongSan.Controllers
 {
     [Area("Admin")]
     [Authorize(Roles = "Admin,Staff")]
-    // [Authorize(Policy = "AdminOnly")]
     public class HomeController : Controller
     {
-        public IActionResult Index() => View();
+        private readonly NongSanContext _context;
+
+        public HomeController(NongSanContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.TotalProducts = _context.SanPhams.Count();
+            ViewBag.TotalCategories = _context.DanhMucs.Count();
+            ViewBag.TotalOrders = _context.DonHangs.Count();
+            ViewBag.TotalRevenue = _context.DonHangs.Sum(d => d.TongTien);
+
+            // Gửi dữ liệu cho biểu đồ (1 giá trị duy nhất)
+            ViewBag.RevenueLabels = new string[] { "Tổng doanh thu" };
+            ViewBag.RevenueValues = new decimal[] { ViewBag.TotalRevenue };
+
+            return View();
+        }
     }
 }
